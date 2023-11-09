e83f8164c0be6fe2258599ff6cc2502dfe6523a5
HuyNLy <huyl.business@gmail.com>
2023-11-08 19:47:34 -0600

    create file

---
+++ SecretBox/SecretBox/SecretBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretBoxApp.swift
+//  SecretBox
+//
+//  Created by Huy Ly on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SecretBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
